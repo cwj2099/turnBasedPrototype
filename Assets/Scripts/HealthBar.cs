ecdc27b1c4949fc95e9e9c50f8d7e28a818f97ce
GameMas <jxie2@wpi.edu>
2021-03-27 10:26:40 -0400

    Good
    Good

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image HealthBar_HP;
+    public Image HealthBar_Effect;
+    float fillAmount = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void healthBar_Update(float givenfillAmount)
+    {
+        fillAmount = givenfillAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBar_HP.fillAmount = fillAmount;
+        if (HealthBar_Effect.fillAmount > HealthBar_HP.fillAmount)
+        {
+            HealthBar_Effect.fillAmount -= 0.003f;
+        }
+        else
+        {
+            HealthBar_Effect.fillAmount = HealthBar_HP.fillAmount;
+        }
+    }
+}
